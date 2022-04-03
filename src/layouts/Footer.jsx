ebaf4354cad2e40c3c947f5d4a5d0484354bb456
Martyna Kroker <martyna1995.k@interia.pl>
2022-04-03 16:30:43 +0200

    Change footer and list-style-type

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 
 function Footer() {
     return (
-        <p>Wszystkie prawa zastrzeżone</p>
+        <>
+            <p>Copyright © 2022 F.U.H. „OXON”</p>
+            <div>
+                <h4>Nasze biuro</h4>
+                <p>ul. Tadeusza Kościuszki 63, 41-503 Chorzów</p>
+                <p><strong>Telefon:</strong> +48 507 133 631, +48 604 958 671</p>
+                <p><strong>E-mail:</strong> oxon_sl@interia.pl</p>
+            </div>
+        </>
     );
 };
 
